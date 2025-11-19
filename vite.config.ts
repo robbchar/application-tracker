5a01b7e10d4a821edf14b422a86ad63f9d7f1b0a
Robb Charnock <robbchar@gmail.com>
2025-11-19 14:44:34 -0800

    setting up env files

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    react({
-      // Use the classic JSX runtime to avoid relying on `react/jsx-dev-runtime`,
-      // which can be problematic with some bundler configurations.
-      jsxRuntime: 'classic',
-    }),
+    react(),
   ],
 })
